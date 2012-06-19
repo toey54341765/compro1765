029699d9e030b96c3a65c3e609e16f19a8d3fd74
Sunisa <khontoey_kak@hotmail.com>
2012-06-19 14:23:26 +0700

    เพิ่ม Programming is fun

--- src/th/ac/nu/student/u54341765/compro/ch01/Helloworld.java
+++ src/th/ac/nu/student/u54341765/compro/ch01/Helloworld.java
@@ -5,5 +5,6 @@ package th.ac.nu.student.u54341765.compro.ch01;
 public class Helloworld {
     public static void main(String []args){
         System.out.println("Hello World");
+        System.out.println("Promgremming is fun.");
     }
-}
+    }
